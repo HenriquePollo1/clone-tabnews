3e99e44dceadde8fe5750467abc50e5ffe43026c
Henrique de Araujo Pollo <56706774+HenriquePollo1@users.noreply.github.com>
2024-06-09 11:15:39 +0000

    adicionando imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-return <><h1>Quem é a namorada mais linda do mundo? </h1><input type="button" /><h1>TE AMO 3</h1></>
+return <><h1>Quem é a namorada mais linda do mundo? </h1>
+        <input type="button" value="Click Me"/>
+        
+        <img src="Imagem do WhatsApp de 2024-06-09 à(s) 08.12.16_2004cc93.jpg" />
+        
+        <h1>TE AMO </h1></>
 }
 
 export default Home;
