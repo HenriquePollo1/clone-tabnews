550a6d60748e8b54f03a779ea7f817b7be1b5e37
Henrique de Araujo Pollo <56706774+HenriquePollo1@users.noreply.github.com>
2024-06-26 11:38:07 +0000

    editorconfig

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 function Home(){
 return <><h1>Quem é a namorada mais linda do mundo? </h1>
-        <input type="button" value="Click Me"/>
-        var batsu = document.createElement("input");  
-        batsu.setAttribute('type', 'button');  
-        <h1>TE AMO </h1></>
+         <h1>TE AMO </h1></>
 }
 //proximo git commit usar o ammend
 export default Home;
