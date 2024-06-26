6a2cafa662948828eec3885ee47523db6d9a1ebf
Henrique de Araujo Pollo <56706774+HenriquePollo1@users.noreply.github.com>
2024-06-26 12:12:03 +0000

    adiciona scriopts ´lintt:check´e ´lint:diff´

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-function Home(){
-return <><h1>Quem é a namorada mais linda do mundo? </h1>
-         <h1>TE AMO </h1></>
+function Home() {
+  return (
+    <>
+      <h1>Quem é a namorada mais linda do mundo? </h1>
+      <h1>TE AMO </h1>
+    </>
+  );
+
+  function teste() {
+    console.log("oie");
+  }
+  function teste() {
+    console.log("oilindoaidnaskjndaskjdakdjnsdjnksd");
+  }
 }
 //proximo git commit usar o ammend
 export default Home;
